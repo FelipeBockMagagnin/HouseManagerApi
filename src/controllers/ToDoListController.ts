31714f038be5dd2029d93556ff30bf1511b4cf08
Felpi <felipebmagagnin@gmail.com>
2020-09-01 22:19:40 -0300

    feat: create todolist/:id get route

--- src/controllers/ToDoListController.ts
+++ src/controllers/ToDoListController.ts
@@ -1,23 +1,33 @@
-class TodoController {
-  index(request, response) {
+import { Request, Response } from 'express'
+
+export default class TodoController {
+  index(req: Request, res: Response) {
+    const id: Number = Number.parseInt(req.params.id);
+
     const todolist = [
       {
+        id: 1,
         name: 'carlos',
         date: '10-03-2020'
       },
       {
+        id: 2,
         name: 'carlos',
         date: '10-03-2020'
       },
       {
+        id: 3,
         name: 'carlos',
         date: '10-03-2020'
       }
     ]
 
-    console.log('recebida requisicao: ', request.route)
-    return response.json(todolist)
+    const item = todolist.find(element => element.id == id);
+
+    if(item === undefined){
+      return res.status(204).send()
+    }
+
+    return res.status(200).json(item);
   }
 }
-
-export default TodoController
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import TodoController from './controllers/ToDoListController'
-const routes = express.Router()
 
-routes.get('/todolist', TodoController.index)
+const routes = express.Router()
+const todoController = new TodoController();
+
+routes.get("/todolist/:id", todoController.index)
 
 export default routes 
