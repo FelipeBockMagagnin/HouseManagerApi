47ef6209e9b04fd5d7d59fea17e08775d7465fc0
Felpi <felipebmagagnin@gmail.com>
2020-09-02 21:43:12 -0300

    refactor: change get todolist method

--- src/controllers/ToDoListController.ts
+++ src/controllers/ToDoListController.ts
@@ -3,14 +3,14 @@ import db from '../../data/db'
 
 export default class TodoController {
   async index(req: Request, res: Response) {
-    const id: Number = Number.parseInt(req.params.id);
+    //const id: Number = Number.parseInt(req.params.id);
     const todos = await db("todolist");
-    const item = todos.find(element => element.id == id);
+    //const item = todos.find(element => element.id == id);
 
-    if(item === undefined){
+    if(todos === undefined){
       return res.status(204).send()
     }
 
-    return res.status(200).json(item);
+    return res.status(200).json(todos);
   }
 }
