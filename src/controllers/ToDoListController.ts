0bece65f7d6230a6a15e51d93744039d32052290
Felpi <felipebmagagnin@gmail.com>
2020-09-02 22:30:55 -0300

    feat: add create and delete todolist methods

--- src/controllers/ToDoListController.ts
+++ src/controllers/ToDoListController.ts
@@ -3,14 +3,43 @@ import db from '../../data/db'
 
 export default class TodoController {
   async index(req: Request, res: Response) {
-    //const id: Number = Number.parseInt(req.params.id);
-    const todos = await db("todolist");
-    //const item = todos.find(element => element.id == id);
+    const todos = await db("todolist").select('*');
 
     if(todos === undefined){
-      return res.status(204).send()
+      return res.status(404).send()
     }
 
     return res.status(200).json(todos);
+  }
+
+  async create(req: Request, res: Response) {
+    const { title, name, description } = req.body;
+
+    const [id] = await db('todolist').insert({
+      "title": title,
+      "name": name,
+      "description": description
+    }).returning('id');
+
+    return res.json({ id });
+  }
+
+  async delete(req: Request, res: Response){
+    const { id } = req.params;
+
+    const todo = await db("todolist")
+      .where("id", id)
+      .select("id")
+      .first();
+
+    if(todo == undefined){
+      return res.status(404).send();
+    }
+
+    await db("todolist")
+      .where("id", id)
+      .delete();
+
+    return res.status(204).send();
   }
 }
