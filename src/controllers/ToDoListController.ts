8f5510dfbad35fed543075c2f2a7da31e788e61e
Felpi <felipebmagagnin@gmail.com>
2020-09-02 21:40:18 -0300

    feat: add knex integration to get todolist method

--- src/controllers/ToDoListController.ts
+++ src/controllers/ToDoListController.ts
@@ -1,28 +1,11 @@
 import { Request, Response } from 'express'
+import db from '../../data/db'
 
 export default class TodoController {
-  index(req: Request, res: Response) {
+  async index(req: Request, res: Response) {
     const id: Number = Number.parseInt(req.params.id);
-
-    const todolist = [
-      {
-        id: 1,
-        name: 'carlos',
-        date: '10-03-2020'
-      },
-      {
-        id: 2,
-        name: 'carlos',
-        date: '10-03-2020'
-      },
-      {
-        id: 3,
-        name: 'carlos',
-        date: '10-03-2020'
-      }
-    ]
-
-    const item = todolist.find(element => element.id == id);
+    const todos = await db("todolist");
+    const item = todos.find(element => element.id == id);
 
     if(item === undefined){
       return res.status(204).send()
