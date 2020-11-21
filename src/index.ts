e896b047f45dfaf8c940034466923fe40ca1526c
Felpi <felipebmagagnin@gmail.com>
2020-11-21 10:34:51 -0300

    config: change PORT to a .env var

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import routes from './routes'
+import 'dotenv/config'
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333)
+app.listen(process.env.PORT ?? 3333)
